8a604534adb42db029003393e22c63194457c1ba
Rafael Jesús Fernández Galleguillos <rafaelfernandezgalleguillos@outlook.com>
2025-11-10 21:58:36 -0300

    Fix Deployment Definitive

--- services/DataService.ts
+++ services/DataService.ts
@@ -1,3 +1,6 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 import { TimelineItem } from "@/components/Timeline";
 import { StatItem } from "@/components/StatsHighlights";
 import { TechStackItem } from "@/components/TechStackGrid";
@@ -7,14 +10,18 @@ import { CarouselItem } from "@/components/Carousel";
 export class DataService {
     
     private async get<T>(fileName: string): Promise<T> {
-        const res = await fetch(`/${fileName}`, {
-            cache: "no-store",
-        });
-        if (!res.ok) {
-            const exception = await res.text();
-            throw new Error(`Error al obtener ${fileName}: ${res.status} - ${exception} `);
+
+         const filePath = path.join(process.cwd(), 'public', fileName);
+
+        try {
+            const fileData = await fs.readFile(filePath, 'utf8');
+            const data = JSON.parse(fileData);
+            return data as T;
+        } catch (error) {
+            console.error(`Error al leer o parsear el archivo ${fileName}:`, error);
+            const message = error instanceof Error ? error.message : String(error);
+            throw new Error(`Error al obtener ${fileName}: ${message}`);
         }
-        return res.json() as Promise<T>;
     }
 
     public getStats(): Promise<StatItem[]> {
